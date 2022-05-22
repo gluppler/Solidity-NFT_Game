4cb15fd8262860c09c9b74622b91dfdbec236767
gluppler <chewzhanhongint@gmail.com>
2022-05-23 00:47:46 +0800

    Frontend

---
+++ Frontend/src/constants.js
@@ -0,0 +1,16 @@
+const CONTRACT_ADDRESS = '0x54656125BF0C6aBAe05409af89621D92C6B13019';
+
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+    return {
+      name: characterData.name,
+      imageURI: characterData.imageURI,
+      hp: characterData.hp.toNumber(),
+      maxHp: characterData.maxHp.toNumber(),
+      attackDamage: characterData.attackDamage.toNumber(),
+    };
+  };
+  
+  export { CONTRACT_ADDRESS, transformCharacterData };
